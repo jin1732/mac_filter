d4d28949afb4c9603120b284814137e867e55774
손희진 <son1732321732@c6r5s8.codyssey.kr>
2026-08-21 16:08:47 +0900

    오류 판정 테스트 순서추가

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import json
 import time
 
 
-def mac_score(pattern, filter_data):
-    score = 0.0
-
-    for i in range(len(pattern)):
-        for j in range(len(pattern[i])):
-            score += pattern[i][j] * filter_data[i][j]
-
-    return score
-
 def measure_performance(filter_a, pattern):
     sizes = [3, 5, 13, 25]
     repeat = 10
